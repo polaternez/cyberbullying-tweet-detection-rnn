6fefe1e61e961be1ec97e31b048de1928a0a5ebe
Polat Ernez <polat_ernez@windowslive.com>
2022-12-22 00:28:58 +0300

    Flask API added

---
+++ Flask_API/app.py
@@ -0,0 +1,70 @@
+import json
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras import layers
+from keras.models import load_model
+import text_cleaning as tc
+
+VOCAB_SIZE = 50000  # vocabulary size
+MAX_LEN = 200
+label_dict = {0: 'not_cyberbullying', 1: 'gender', 2: 'religion', 3: 'other_cyberbullying', 4: 'age', 5: 'ethnicity'}
+
+# vectorization layer is adapted
+cleaned_tweets_df = pd.read_csv("cleaned_tweets.csv")
+cleaned_tweets_df.dropna(inplace=True)
+vectorize_layer = tf.keras.layers.TextVectorization(
+    max_tokens=VOCAB_SIZE,
+    standardize='lower',
+    output_mode='int',
+    output_sequence_length=MAX_LEN
+)
+
+vectorize_layer.adapt(cleaned_tweets_df["clean_text"])
+
+# Load model
+model = load_model("saved_models/cyberbullying_lstm.h5")
+
+# WSGI
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    new_data = request.form["tweet_text"]
+    temp_df = pd.DataFrame(np.array(new_data).reshape(-1, 1), columns=['tweet_text'])
+    clean_text = temp_df["tweet_text"].apply(tc.clean_data)
+    vectorized_text = vectorize_layer(clean_text)
+    
+    prediction = model.predict(vectorized_text.numpy())
+    output = np.argmax(prediction, axis=1)
+    output_text = label_dict[int(output)]
+    
+    return render_template("index.html", prediction_text=output_text, tweet=f"<< {new_data} >>")
+        
+@app.route("/predict_api", methods=["POST"])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    request_json = request.get_json()
+    new_data = request_json["input"]
+    temp_df = pd.DataFrame(np.array(new_data).reshape(-1, 1), columns=['tweet_text'])
+    clean_text = temp_df["tweet_text"].apply(tc.clean_data)
+    vectorized_text = vectorize_layer(clean_text)
+
+    prediction = model.predict(vectorized_text.numpy())
+    output = np.argmax(prediction, axis=1)
+    output_text = [label_dict[x] for x in output]
+    
+    response = json.dumps({'response': str(output_text)})
+    return response, 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
